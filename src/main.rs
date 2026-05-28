0cf3fa9465ef18cce33652007d62c3ec9c68fa48
GavMason <gmasonwrk@gmail.com>
2026-05-28 06:05:12 -0500

    update cli logo to current branding

--- src/main.rs
+++ src/main.rs
@@ -164,15 +164,39 @@ fn format_date(iso: &str) -> String {
 fn print_text_report(inv: &ModelInvestigation, elapsed: std::time::Duration) {
     println!();
     let logo = [
+        r"      :+#%%%%%%%%*=. .-*#%%%%%%%#*-.   .:=++++=-:",
+        r"   .+%%*-:.    .:=#@%@%+-..   ..-+#%*+%@@@@@@@@@@%*-",
+        r"  +@#:           .*@*@%:          .%@@@@@@@@@@@@@@@@%=",
+        r" #@=            :%@: .#@-        -@@@@@@:.+@@@@@@@@@@@*",
+        r"*@=             %@:    %@.      .@@@@@@@. -*==*@@@@@@@@+",
+        r"@@             :@#     =@=      +@@@@@@@. .++: .%@@@@@@@",
+        r"@@             :@#     =@=      +@@@@@@@. -@@#  *@@@@@@@",
+        r"*@=             %@:    %@.      .@@@@@@@. .==. -@@@@@@@+",
+        r" #@=            :%@: .#@-        -@@@@@@##%%**%@@@@@@@*",
+        r"  +@#:           .*@*@%:          .%@@@@@@@@@@@@@@@@%=",
+        r"   .+%%*-:.    .:=#@%@%+-..   ..-+#%*+%@@@@@@@@@@%*-",
+        r"      :+#%%%%%%%%*=. .-*#%%%%%%%#*-.   .:=++++=-:",
+    ];
+    let width = logo.iter().map(|l| l.len()).max().unwrap_or(0);
+    for line in logo {
+        println!("{}", line.color(LAVENDER).bold());
+    }
+    println!();
+    let name = [
         r"      __",
         r"     / /  ___  ___  ___ _",
         r"    / _ \/ _ \/ _ \/ _ `/",
         r"   /_.__/\___/_//_/\_,_/",
     ];
-    for line in logo {
-        println!("  {}", line.color(LAVENDER).bold());
+    let name_width = name.iter().map(|l| l.len()).max().unwrap_or(0);
+    let name_pad = (width - name_width) / 2;
+    for line in name {
+        println!("{:pad$}{}", "", line.color(LAVENDER).bold(), pad = name_pad);
     }
-    println!("  {}", "─── provenance explorer ───".dimmed());
+    let subtitle = "─── provenance explorer ───";
+    let visual_len = subtitle.chars().count();
+    let pad = (width - visual_len) / 2;
+    println!("{:pad$}{}", "", subtitle.dimmed(), pad = pad);
 
     let hf_url = format!("https://huggingface.co/{}", inv.model_id);
     let model_link = hyperlink(&hf_url, &inv.model_id);
